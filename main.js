31b002148cdafa22cd621f82678c9ad6bdd48e61
Zineb <zeynepturna1992@gmail.com>
2025-11-18 22:11:36 +0100

    Creation du HTML page et style utilisant Tailwindcss

---
+++ main.js
@@ -0,0 +1,12 @@
+const AddWorker = document.getElementById("AddWorker");
+const Forml = document.getElementById("forml");
+
+
+
+
+
+
+
+AddWorker.addEventListener('click', () =>{
+    Forml.classList.toggle('hidden')
+})
