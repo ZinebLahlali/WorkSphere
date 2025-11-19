c6ff0009eb89a3fb370efac7954e045dba3550f8
Zineb <zeynepturna1992@gmail.com>
2025-11-19 22:22:59 +0100

    creation d'un tableau pour stocker les informations de chauqe employer

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const Forml = document.getElementById("forml");
 const MonForm = document.getElementById("monForm");
 const NomePrenom = document.getElementById("nomPrenom");
 const Role = document.getElementById("role");
-const Photo = document.getElementById("photo");
+const Url = document.getElementById("photo");
 const image = document.getElementById("img");
 const addressEmail = document.getElementById("email");
 const phone = document.getElementById("phone");
@@ -19,8 +19,10 @@ const Serveurs = document.getElementById("serveurs");
 const conference = document.getElementById("conference");
 const Reception = document.getElementById("reception");
 const experience = document.getElementById("expriences");
+const Enregistrer = document.getElementById("enregistrer");
+const Card = document.getElementById("card");
 
-
+const Globalarry = [] 
 
 
 
@@ -35,26 +37,65 @@ AjouterExp.addEventListener('click', () => {
                             <label for="Exper">Expériences</label>
                         </div>
                         <div>
-                         <input class="border-2 rounded-lg p-2" type="text" name="metier" id="metier" required>
+                         <input class="border-2 rounded-lg p-2" type="text" name="metier" >
                         </div>
                         <div>
                             <label for="descreption">Description</label>
                         </div>
                         <div class="mt-2">
-                         <textarea  class="border-2 rounded-lg p-2" name="descreption" id="descreption" required></textarea>
+                         <textarea  class="border-2 rounded-lg p-2" name="descreption"  ></textarea>
                         </div>
                        <div class="flex gap-4 mt-4">
                         <div class="flex flex-rows gap-1">
-                         <span>à</span> <input class="border-2 rounded-lg p-1 h-fit" type="date" name="dateDebut" id="dateDebut" required/>
+                         <span>à</span> <input class="border-2 rounded-lg p-1 h-fit" type="date" name="dateDebut">
                        </div>
                        <div class="flex flex-rows gap-1">
-                        <span>de</span> <input class="border-2 rounded-lg p-1 h-fit" type="date" name="dateFin" id="dateFin" required/>
+                        <span>de</span> <input class="border-2 rounded-lg p-1 h-fit" type="date" name="dateFin">
                        </div>
                        </div>
 </div>
 `;
     experience.appendChild(printexperience);
 })
+
+MonForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const object = {
+        Nome: NomePrenom.value,
+        Rôle: Role.value,
+        Photo:Url.value,
+        Image: image.value,
+        Email: addressEmail.value,
+        telephone: phone.value,
+        Metier: work.value,
+        Descreption: Descreption.value,
+        DateDebut: dateDebut.value,
+        DateFin: dateFin.value,
+
+
+    }
+    Forml.classList.add('hidden');
+
+    Globalarry.push(object);
+
+    console.log(Globalarry);
+    
+    const printData = document.createElement('div');
+    printData.innerHTML = ` 
+    <img src=${} alt="">
+    <div>
+     <p>${NomePrenom}</p>
+     <p>${Role}</p>
+     <p>${phone}</p>
+     <p>${work}</p>
+
+    </div>
+    `
+    Card.appendChild(printData);
+
+});
+
+
 
 
 
